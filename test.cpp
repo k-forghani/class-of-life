288a4647d05f8d07cfa30c588aed79b178099090
Kazem Forghani <kaz.forghani@gmail.com>
2023-03-22 00:49:08 +0330

    Added some tests

--- test.cpp
+++ test.cpp
@@ -5,5 +5,24 @@
 using namespace std;
 
 int main () {
+    /* transformRNAtoDNA() */
+
+    cout << "transformRNAtoDNA()" << endl;
+
+    Genome genome(
+        "gactacggat",
+        "gatacacacc",
+        "gtggggagct"
+    );
+    pair<Strand, Strand> dna = genome.transformRNAtoDNA();
+    cout << dna.first.getStrand() << endl << dna.second.getStrand() << endl;
+
+    /* mutateSmallScaleDNA() */
+
+    cout << endl << "mutateSmallScaleDNA()" << endl;
+
+    genome.mutateSmallScaleRNA('g', 'a', 2);
+    cout << genome.getRNA().getStrand() << endl;
+
     return 0;
 }
