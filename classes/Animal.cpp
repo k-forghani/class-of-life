9dddb396b9d92e261d7e4973ae823c8e59a4265e
Kazem Forghani <kaz.forghani@gmail.com>
2023-04-06 18:14:31 +0330

    Refactored Animal.cpp

--- classes/Animal.cpp
+++ classes/Animal.cpp
@@ -14,11 +14,10 @@ void Animal::killBadChromosomes () {
     while (i != chromosomes.end()) {
         Chromosome c(**i);
 
-        if (c.isAbnormal()) {
+        if (c.isAbnormal())
             i = chromosomes.erase(i);
-        } else {
+        else
             ++i;
-        }
     }
     number = chromosomes.size();
 }
@@ -32,27 +31,24 @@ double Animal::getGeneticSimilarity (const Animal& animal) const {
 
         for (auto j = chromosomes.begin(); j != chromosomes.end(); ++j) {
             pair<Strand, Strand> dna = (**j).getChromosome();
-            if (i == 0 || i == 1) {
+            if (i == 0 || i == 1)
                 a.push_back(dna.first.getStrand());
-            } else if (i == 2 || i == 3) {
+            else if (i == 2 || i == 3)
                 a.push_back(dna.second.getStrand());
-            }
         }
         
         for (auto j = animal.chromosomes.begin(); j != animal.chromosomes.end(); ++j) {
             pair<Strand, Strand> dna = (**j).getChromosome();
-            if (i == 0 || i == 2) {
+            if (i == 0 || i == 2)
                 b.push_back(dna.first.getStrand());
-            } else if (i == 1 || i == 3) {
+            else if (i == 1 || i == 3)
                 b.push_back(dna.second.getStrand());
-            }
         }
 
         double score = computeMHS(a, b);
 
-        if (score > similarity) {
+        if (score > similarity)
             similarity = score;
-        }
     }
     
     return similarity;
@@ -77,36 +73,31 @@ Animal Animal::reproduceAsexually () const {
         );
     }
 
-    for (int i = 0; i < number; i++) {
+    for (int i = 0; i < number; i++)
         (child -> chromosomes).pop_back();
-    }
 
     child -> number = number;
 
-    if (Animal::getGeneticSimilarity(*child) <= SIMILARITY_THRESHOLD) {
+    if (Animal::getGeneticSimilarity(*child) <= SIMILARITY_THRESHOLD)
         return Animal::reproduceAsexually();
-    } else {
+    else
         return *child;
-    }
 }
 
 bool operator== (const Animal& a1, const Animal& a2) {
-    if (a1.number != a2.number) {
+    if (a1.number != a2.number)
         return false;
-    }
 
-    if (a1.getGeneticSimilarity(a2) > SIMILARITY_THRESHOLD) {
+    if (a1.getGeneticSimilarity(a2) > SIMILARITY_THRESHOLD)
         return true;
-    }
 
     return false;
 }
 
 Animal operator+ (const Animal& a1, const Animal& a2) {
-    if (a1.number != a2.number) {
+    if (a1.number != a2.number)
         return Animal((int)((a1.number + a2.number) / 2));
-    }
-
+    
     Animal c1 = a1.reproduceAsexually();
     Animal c2 = a2.reproduceAsexually();
 
@@ -140,11 +131,10 @@ Animal operator+ (const Animal& a1, const Animal& a2) {
         );
     }
 
-    if (a1.getGeneticSimilarity(*child) > 0.7 && a2.getGeneticSimilarity(*child) > 0.7) {
+    if (a1.getGeneticSimilarity(*child) > SIMILARITY_THRESHOLD && a2.getGeneticSimilarity(*child) > SIMILARITY_THRESHOLD)
         return *child;
-    } else {
+    else
         return a1 + a2;
-    }
 }
 
 /* Virus */
@@ -177,20 +167,18 @@ bool Virus::isPathogenic (const Animal& animal) const {
 
         for (auto j = animal.chromosomes.begin(); j != animal.chromosomes.end(); ++j) {
             pair<Strand, Strand> dna = (**j).getChromosome();
-            if (i == 0) {
+            if (i == 0)
                 strings.push_back(dna.first.getStrand());
-            } else {
+            else
                 strings.push_back(dna.second.getStrand());
-            }
         }
         
         string lcs = findLCS(strings);
 
         string r = rna -> getStrand();
 
-        if (lcs != "" && (findPattern(r, lcs) || findPattern(r, getStrandComplement(lcs)))) {
+        if (lcs != "" && (findPattern(r, lcs) || findPattern(r, getStrandComplement(lcs))))
             return true;
-        }
     }
     
     return false;
